47ca93e7afabb04da26fe5897322fec348ae20ef
BIBLEOFGONGBOO <budongsanedu@gmail.com>
2026-09-06 09:50:00 -0700

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,10 +1,30 @@
 window.GONGBOO_CATALOG = Object.freeze({
   groups: [
-    { name: 'Bible', type: 'system', urlKey: 'bible' },
-    { name: 'License', type: 'system', urlKey: 'license' }
+    {
+      name: 'Bible',
+      type: 'system',
+      urlKey: 'bible'
+    },
+    {
+      name: 'License',
+      type: 'system',
+      urlKey: 'license'
+    },
+    {
+      name: 'Easy Learning',
+      type: 'system',
+      urlKey: 'easyLearning'
+    }
   ],
+
   urls: {
-    bible: 'https://biblegongboo.github.io/bible/supabase/app/',
-    license: 'https://biblegongboo.github.io/license/app/'
+    bible:
+      'https://bibleofgongboo.github.io/bible/supabase/app/',
+
+    license:
+      'https://bibleofgongboo.github.io/license/app/',
+
+    easyLearning:
+      'https://bibleofgongboo.github.io/anne/'
   }
 });
